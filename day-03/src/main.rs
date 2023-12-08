59d5bd68e3b482852e1785db6668ac16d6bd9dc0
NullCube <nullcub3@gmail.com>
2023-12-08 01:13:03 -0800

    Why doesnt this work

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -1,7 +1,8 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, usize};
 
 type BluePrint = HashMap<usize, Vec<char>>;
 
+#[derive(Clone, Debug)]
 struct NumberAddress {
     number: usize,
     x: Vec<usize>,
@@ -9,8 +10,8 @@ struct NumberAddress {
 }
 
 fn main() {
-    // let input = include_str!("../input");
-    let input = include_str!("../example");
+    let input = include_str!("../input");
+    // let input = include_str!("../example");
     let data = input.lines();
     let mut blueprint: BluePrint = HashMap::new();
 
@@ -18,11 +19,7 @@ fn main() {
     for (i, line) in data.enumerate() {
         let linechar: Vec<char> = line.chars().collect();
         blueprint.insert(i, linechar.clone());
-        // println!("{:?}, {:?}", i, linechar);
     }
-    // for (i, map) in blueprint {
-    //     println!("{:?}, {:?}", i, map);
-    // }
 
     // Sweep for number addresses
     let mut numbers: Vec<NumberAddress> = vec![];
@@ -50,37 +47,42 @@ fn main() {
 
     // Check numbers for actual numbers
     let mut verified_numbers: Vec<NumberAddress> = vec![];
-    for (i, num) in numbers.iter().enumerate() {
-        println!("Num: {:?}, x: {:?}, y: {:?}", num.number, num.x, num.y);
+    for (_i, num) in numbers.iter().enumerate() {
+        // println!("Num: {:?}, x: {:?}, y: {:?}", num.number, num.x, num.y);
 
-        let mut sec_end = num.x.last().unwrap().to_owned();
-        if sec_end < blueprint[&num.y].len() {
-            sec_end += 1;
-        }
+        let sec_end = num
+            .x
+            .last()
+            .unwrap()
+            .to_owned()
+            .clamp(0, blueprint[&num.y].len() - 1)
+            .saturating_add(1);
+        //println!("sec_end: {:?}", sec_end);
 
-        let mut sec_first = num.x.first().unwrap().to_owned();
-        sec_first = sec_first.saturating_sub(1);
+        let sec_first = num.x.first().unwrap().to_owned().saturating_sub(1);
 
-        let line_next = if num.y < blueprint.len() - 1 {
-            num.y + 1
-        } else {
-            num.y
-        };
-        println!("Lines: {}", blueprint.len());
-        println!("Line Next {}", line_next);
-        println!("Line Past {}", num.y.saturating_sub(1));
+        let line_y = num.y.clamp(0, blueprint.len() - 2);
+        // println!("bp len: {:?}", blueprint.len());
+        // println!("line: {:?}", line_y);
+        // println!("line_next: {:?}", line_y.saturating_add(1));
 
-        for y in [num.y.saturating_sub(1), num.y, line_next] {
-            println!("Y: {:?}", y);
-            let bp: String = blueprint[&y][sec_first..=sec_end].iter().collect();
-            println!("Section: {:?}", bp);
-            if bp.chars().any(|c| c.is_ascii_punctuation()) {
-                println!("Had symbols: {:?}", bp);
+        'linecheck: for y in [line_y.saturating_sub(1), line_y, line_y.saturating_add(1)] {
+            //println!("y: {:?}", y);
+            for c in blueprint[&y][sec_first..=sec_end].iter() {
+                if c.is_ascii_digit() || c == &'.' {
+                } else {
+                    verified_numbers.push(num.clone());
+                    break 'linecheck;
+                }
             }
         }
-        //println!("Block: {:?}", block);
-
-        println!("Section end: {:?}", sec_end);
-        println!("Section first: {:?}", sec_first);
     }
+    // println!("verified_numbers: {:?}", verified_numbers);
+    let mut bp_sum = 0;
+    for vn in verified_numbers {
+        // println!("Verified Number: {:?}", vn.y);
+        bp_sum += vn.number;
+    }
+
+    println!("bp_sum: {:?}", bp_sum);
 }
